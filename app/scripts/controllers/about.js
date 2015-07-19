64a9342fad4368ac6660d9a70199598d4bc953ad
Enrique Ramirez <enrique.ramirez35@gmail.com>
2015-07-18 23:48:48 -0600

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clickthroughApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clickthroughApp
+ */
+angular.module('clickthroughApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
