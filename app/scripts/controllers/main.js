a21da06ee5590992a41ad5edaa416a6fb987d458
Enrique Ramirez <enrique.ramirez35@gmail.com>
2015-07-20 09:34:10 -0600

    tested json files
    tested son files from config

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,24 +8,15 @@
  * Controller of the clickthroughApp
  */
 angular.module('clickthroughApp')
-  .controller('MainCtrl', function($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', '$http',function($scope, $http) {
 
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: 'http://placekitten.com/' + newWidth + '/300',
-        text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-      });
-    };
-    for (var i = 0; i < 4; i++) {
-      $scope.addSlide();
-    }
+    $http.get('testData/hcm.json').then(
+      function(response) {
+        console.log(response.data);
 
-  });
+        $scope.sections = response.data;
+      }
+    );
+
+
+  }]);
